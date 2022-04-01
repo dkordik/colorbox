ad3ae7a41b6b448df2cafeba50dff18387d1af3c
Dan Kordik <dan.kordik@cision.com>
2022-03-31 20:16:49 -0500

    debug.js: update sample images to newer working links

--- debug.js
+++ debug.js
@@ -58,7 +58,7 @@ var ColorBoxDebugger = {
 						});
 					}
 				})
-				.catch(() => {
+				.catch((err) => {
 					console.error("Error with " + imageUrl + "\n", err);
 				});
 		})
@@ -66,14 +66,9 @@ var ColorBoxDebugger = {
 }
 
 var imageUrls = [
-	"https://consequenceofsound.files.wordpress.com/2015/02/braids-deep-in-the-iris.jpg?w=806&h=806",
-	"http://a2.mzstatic.com/us/r30/Music/v4/ca/07/78/ca077842-b7e6-48d7-60ac-fdbd3bc637cc/677517007763.1200x1200-75.jpg",
-	"http://a3.mzstatic.com/us/r30/Music6/v4/44/68/42/4468426b-e5b3-ba9d-c5be-d6b1473c4e18/FCL99_1600.1200x1200-75.jpg",
-	"http://a1.mzstatic.com/us/r30/Music/v4/cd/b0/a3/cdb0a392-716b-554c-d851-a76837208439/656605512662.1200x1200-75.jpg",
-	"http://a4.mzstatic.com/us/r30/Music2/v4/39/f4/b5/39f4b5d4-ac76-c9f8-f2a4-b9171ad8a7ec/cover1200x1200.jpeg",
-	"http://a2.mzstatic.com/us/r30/Music7/v4/bf/fb/06/bffb06e6-9ce7-845b-095a-60fc180bc219/cover1200x1200.jpeg",
-	"http://a2.mzstatic.com/us/r30/Music3/v4/a8/15/1d/a8151d4b-031f-58de-22b6-e075574d3355/cover1200x1200.jpeg",
-	"http://a2.mzstatic.com/us/r30/Music1/v4/7f/05/7d/7f057d61-5e72-38fd-b26c-5f84fa7058d7/cover1200x1200.jpeg"
+  "https://images.unsplash.com/photo-1521133573892-e44906baee46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+	"https://images.unsplash.com/photo-1560942800-87cef0f0bc92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+	"https://images.unsplash.com/photo-1467164616789-ce7ae65ab4c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
 ];
 
 ColorBoxDebugger.renderTestbed(imageUrls);
