219087732110ee714f95127ff2e3f4ca9b4c19ff
Dan Kordik <dan.kordik@cision.com>
2022-03-31 20:16:30 -0500

    debugger: update gulp/browser-sync to more current versions that run on latest Node

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,29 @@
 var gulp = require('gulp');
 var gutil = require('gutil')
 var spawn = require('child_process').spawn;
-var browserSync = require('browser-sync');
+var browserSync = require("browser-sync").create();
 
-gulp.task('render', function () {
+const render = (cb) => {
 	var nodeProcess = spawn('node', ['debug.js'], { stdio: 'inherit' });
 	nodeProcess.on('close', function (exitCode) {
 		if (exitCode == 8) {
 			gutil.log('Error detected, waiting for changes...');
 		}
+		cb();
 	});
-});
+};
+
+const reload = (cb) => {
+	browserSync.reload("index.html");
+	cb();
+}
 
 gulp.task('debug', function () {
-	browserSync({
+	browserSync.init({
 		notify: false,
-		server: true
+		server: '.'
 	});
 
-	gulp.watch('index.html', browserSync.reload);
-	gulp.watch(["index.js", "debug.js"], ["render"]);
+	gulp.watch("index.html", reload);
+	gulp.watch(["index.js", "debug.js"], gulp.series(render, reload));
 });
